de8af0ab5237ac2b7830351a7c876b2055c6c897
Ruterana <ncharlene38@gmail.com>
2019-09-01 18:58:52 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GithubFormComponent } from './github-form/github-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GithubFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
