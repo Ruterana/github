3c1c99cbe6a4c3f70e538a145b0bb231424589f2
Ruterana <ncharlene38@gmail.com>
2019-09-04 20:09:31 +0200

    i make change small changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,51 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component'
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     GithubFormComponent,
-//     ProfileComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { DateCountPipe } from './date-count.pipe';
 import { UserFormComponent } from './user-form/user-form.component';
 import { UpperCasePipe } from './upper-case.pipe';
-import { StrikethroughDirective } from './strikethrough.directive';
 import { GithDirective } from './gith.directive';
 
-// import { DateCountPipe } from './date-count.pipe';
+
 @NgModule({
- declarations: [
-   AppComponent,
-   ProfileComponent,
-   DateCountPipe,
-   UserFormComponent,
-   UpperCasePipe,
-   StrikethroughDirective,
-   GithDirective,
-  //  DateCountPipe,
- ],
- imports: [
-   BrowserModule,
-   AppRoutingModule,
-   HttpClientModule,
-   FormsModule
-  //  AppComponent,
- 
- ],
- providers: [],
- bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+
+    UserFormComponent,
+    UpperCasePipe,
+
+    GithDirective,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -19,16 +19,14 @@ import { Component, OnInit } from '@angular/core';
 import {User}from '../user';
 import {HttpClient} from '@angular/common/http';
 import {HttpRequestService}from '../repos-request/http-request.service'
-import { Repository } from '../repository';
 @Component({
- selector: 'app-user-profile',
- templateUrl: './user-profile.component.html',
+ selector: 'app-profile',
+ templateUrl: './profile.component.html',
  providers:[HttpRequestService],
- styleUrls: ['./user-profile.component.css']
+ styleUrls: ['./profile.component.css']
 })
-export class UserProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit {
  Users:User;
- repos:Repository;
  addNew(user){
    this.reposRequest.reposRequest(user)
    this.repos=this.reposRequest.repos
